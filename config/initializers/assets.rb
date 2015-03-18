b6f15cb5bab4bd5c12d4f2f6530ab4a97c71a458
Ariel Pérez <arielperezch@gmail.com>
2015-03-18 05:41:56 -0400

    estaba usando el .coffee en vez de .js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 %w( controller_one controller_two controller_three ).each do |controller|
-  Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css"]
+  Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
 end
